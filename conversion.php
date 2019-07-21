1211645870604a02e5bb96a76c2479bbae88af98
ggrando <51934321+ggrando@users.noreply.github.com>
2019-07-21 16:41:37 -0300

    Update conversion.php
    Removendo retornos desnecessários.

--- conversion.php
+++ conversion.php
@@ -8,12 +8,6 @@ function conversion(){
 
 global $nome, $email, $token;
 
-echo "o valor é: ";
-echo $nome;
-echo "<br>";
-echo "o valor do e-mail é: ";
-echo $email;
-echo "<br>";
 
 // Primeiro você deve instanciar um array com os campos que você quer enviar:
 
@@ -55,19 +49,10 @@ $server_output = curl_exec ($ch);
 
 curl_close ($ch);
 
-echo "Esse formulário foi integrado via API 2.0 do RD Station Marketing ";
-
-echo "<br>";
-
 
  
 //faz o parsing na string, gerando um objeto PHP
 $obj = json_decode($server_output);
-
-echo "<br>";
-echo "O identificador dessa conversão é: ";
-echo "$obj->event_uuid";
-echo "<br>";
 
 }
 
